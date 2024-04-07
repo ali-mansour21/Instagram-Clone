aa2175cfebe8f2dc038a25308879ecd5e82a7abd
Ali Mansour <alialhadimansour21@gmail.com>
2024-04-07 21:26:13 +0300

    Implement authentication using JWT

---
+++ backend/app/Http/Controllers/auth/AuthController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Validation\Rule;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:50'],
+            'user_name' => ['required', 'string', Rule::unique('users', 'user_name')],
+            'email' => ['required', 'string', 'email', 'max:255', Rule::unique('users', 'email')],
+            'password' => ['required', 'string', 'min:6', 'max:18'],
+            'profile_image' => ['image', 'mimes:jpeg,png,jpg,gif,svg',]
+        ]);
+        $user = new User();
+        $user->name = $request->name;
+        $user->user_name = $request->user_name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        if ($request->has('profile_image')) {
+            $image_name = Str::random(32) . "." . $request->profile_image->getClientOriginalExtension();
+            Storage::disk('public')->put($image_name, file_get_contents($request->profile_image));
+            $user->profile_image = $image_name;
+        }
+
+        $user->save();
+
+        $token = Auth::login($user);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User created successfully',
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        $credentials = $request->only('email', 'password');
+
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized',
+            ], 401);
+        }
+
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorization' => [
+                'token' => $token,
+                'type' => 'bearer',
+            ]
+        ]);
+    }
+    public function logout()
+    {
+        Auth::logout();
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Successfully logged out',
+        ]);
+    }
+}
