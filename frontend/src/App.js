dcc57c9b3ea9bb94fffde9644530be29e156ce86
Ali Mansour <alialhadimansour21@gmail.com>
2024-04-09 01:30:40 +0300

    Implement Sidebar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./pages/Auth/Components/LoginForm";
 import SignupForm from "./pages/Auth/Components/SignupForm";
+import Home from "./pages/Home/index";
 function App() {
   return (
     <div className="App">
@@ -8,6 +9,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginForm />} />
           <Route path="/signup" element={<SignupForm />} />
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/pages/Home/Components/SideBar.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import "../Styles/sidebar.css";
+import logo from "../../../assets/Instagram_logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faCompass,
+  faEnvelope,
+  faHeart,
+  faHouse,
+  faMagnifyingGlass,
+  faPlay,
+  faSquarePlus,
+} from "@fortawesome/free-solid-svg-icons";
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <img srcSet={logo} alt="Logo" />
+      <div className="menu">
+        <ul>
+          <li>
+            <FontAwesomeIcon icon={faHouse} />
+            Home
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faMagnifyingGlass} />
+            Search
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faCompass} />
+            Explore
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faPlay} />
+            Reels
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faEnvelope} />
+            Messages
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faHeart} />
+            Notifications
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faSquarePlus} />
+            Create
+          </li>
+          <li>Profile</li>
+        </ul>
+      </div>
+      <div className="footer">
+        <ul>
+          <li>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25"
+              />
+            </svg>
+            Thread
+          </li>
+          <li>
+            <FontAwesomeIcon icon={faBars} />
+            More
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
