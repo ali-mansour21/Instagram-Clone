acaef84125090c2026697fd77e693542af879376
Ali Mansour <alialhadimansour21@gmail.com>
2024-04-09 20:15:40 +0300

    Implement the hpme page html css

--- frontend/src/pages/Auth/Components/SignupForm.jsx
+++ frontend/src/pages/Auth/Components/SignupForm.jsx
@@ -7,52 +7,54 @@ const SignupForm = () => {
   return (
     <>
       <>
-        <div className="container">
-          <img srcSet={logo} alt="" />
-          <h3>Sign up to see photos and videos from your friends.</h3>
-          <button>Log in with Facebook</button>
-          <div className="line-2"></div>
-          <div className="form-container">
-            <form>
-              <div>
-                <input type="text" placeholder=" Email" />
+        <div className="main-section">
+          <div className="container">
+            <img srcSet={logo} alt="" />
+            <h3>Sign up to see photos and videos from your friends.</h3>
+            <button>Log in with Facebook</button>
+            <div className="line-2"></div>
+            <div className="form-container">
+              <form>
+                <div>
+                  <input type="text" placeholder=" Email" />
+                </div>
+                <div>
+                  <input type="text" placeholder="Full name" />
+                </div>
+                <div>
+                  <input type="text" placeholder="Username" />
+                </div>
+                <div>
+                  <input type="password" placeholder="Passowrd" />
+                </div>
+                <div>
+                  <button>Sign up</button>
+                </div>
+              </form>
+              <div className="info">
+                <p>
+                  People who use our service may have uploaded your contact
+                  information to Instagram. Learn More
+                </p>
+                <p>
+                  By signing up, you agree to our Terms , Privacy Policy and
+                  Cookies Policy .
+                </p>
               </div>
-              <div>
-                <input type="text" placeholder="Full name" />
-              </div>
-              <div>
-                <input type="text" placeholder="Username" />
-              </div>
-              <div>
-                <input type="password" placeholder="Passowrd" />
-              </div>
-              <div>
-                <button>Sign up</button>
-              </div>
-            </form>
-            <div className="info">
-              <p>
-                People who use our service may have uploaded your contact
-                information to Instagram. Learn More
-              </p>
-              <p>
-                By signing up, you agree to our Terms , Privacy Policy and
-                Cookies Policy .
-              </p>
             </div>
           </div>
-        </div>
-        <div className="signup">
-          <p>
-            Have an account?{" "}
-            <span
-              onClick={() => {
-                navigate("/");
-              }}
-            >
-              Log in
-            </span>
-          </p>
+          <div className="signup">
+            <p>
+              Have an account?{" "}
+              <span
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                Log in
+              </span>
+            </p>
+          </div>
         </div>
       </>
     </>
